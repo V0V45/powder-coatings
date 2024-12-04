8ba10739998ed6edc05f6c3cb4eaa25bd9dfe33b
V0V45 <118116637+V0V45@users.noreply.github.com>
2024-12-04 21:59:02 +0300

    Added mouseover

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,23 @@
 import classes from "./header.module.css";
 import logo from "../../images/logo.svg";
+import logoHover from "../../images/logo-hover.svg";
 import HeaderLink from "./header-link/header-link";
 import HeaderButton from "./header-button/header-button";
 
 export default function Header() {
+    function handleMouseOver(event) {
+        event.target.src = logoHover;
+    }
+
+    function handleMouseOut(event) {
+        event.target.src = logo;
+    }
+
     return (
         <header>
-            <img src={logo} alt="Primatek" className={classes.logo} />
+            <div className={classes.logoContainer}>
+                <img src={logo} alt="Primatek" className={classes.logo} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} />
+            </div>
             <div className={classes.links}>
                 <HeaderLink link="/" space>Products</HeaderLink>
                 <HeaderLink link="/" space>Service</HeaderLink>
