2598264dd6499f6c1e1e8a365f9cef556f46cf10
V0V45 <118116637+V0V45@users.noreply.github.com>
2025-01-06 00:25:01 +0300

    Added paddingLeft

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -6,10 +6,10 @@ export default function Banner() {
 
     return (
         <section className={classes.container}>
-            <BannerH1 gridAreaTitleNumber={1} border>Powder</BannerH1>
+            <BannerH1 gridAreaTitleNumber={1} border paddingLeft paddingTop>Powder</BannerH1>
             <div className={classes.border11} />
             <div className={classes.border12} />
-            <BannerH1 gridAreaTitleNumber={2}>Coatings</BannerH1>
+            <BannerH1 gridAreaTitleNumber={2} paddingLeft>Coatings</BannerH1>
             <div className={classes.border21} />
             <p className={classes.caption}>You have experience in powder coatings and we produce coatings that corresponds your experience</p>
             <ContactUsButton gridAreaName={"contactUs"} />
