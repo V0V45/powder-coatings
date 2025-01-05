70c5d3c1bdd9d44f2a001729ec2ed2f4c2eaece6
V0V45 <118116637+V0V45@users.noreply.github.com>
2025-01-06 00:24:40 +0300

    Added parrot block

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/header/header';
 import Banner from './components/banner/banner';
+import Parrot from './components/parrot/parrot';
 
 export default function App() {
   return (
     <>
       <Header />
       <Banner />
+      <Parrot />
     </>
   );
 }
