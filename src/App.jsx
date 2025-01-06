436c1dfdf95fa7035acec5c864880b9d571b1030
V0V45 <118116637+V0V45@users.noreply.github.com>
2025-01-07 00:34:58 +0300

    Added whoWeAre block

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/header/header';
 import Banner from './components/banner/banner';
 import ColorfulBlock from './components/colorful-block/colorful-block';
+import WhoWeAre from './components/who-we-are/who-we-are';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Header />
       <Banner />
       <ColorfulBlock />
+      <WhoWeAre />
     </>
   );
 }
