794d2f39d681c5932a64c6ba127fbc30764f419a
V0V45 <118116637+V0V45@users.noreply.github.com>
2025-01-07 00:35:45 +0300

    TextCaption refactoring

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,6 +1,7 @@
 import classes from "./banner.module.css";
 import BannerH1 from "./banner-h1/banner-h1";
 import ContactUsButton from "../contact-us-button/contact-us-button";
+import TextCaption from "../text/text-caption";
 
 export default function Banner() {
 
@@ -11,7 +12,7 @@ export default function Banner() {
             <div className={classes.border12} />
             <BannerH1 gridAreaTitleNumber={2} paddingLeft>Coatings</BannerH1>
             <div className={classes.border21} />
-            <p className={classes.caption}>You have experience<br />in powder coatings and we<br />produce coatings that<br />corresponds your<br />experience</p>
+            <TextCaption classes={classes.caption}>You have experience<br />in powder coatings and we<br />produce coatings that<br />corresponds your<br />experience</TextCaption>
             <ContactUsButton gridAreaName={"contactUs"} />
         </section>
     );
