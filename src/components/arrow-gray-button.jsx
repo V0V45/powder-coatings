39d124d4cba0e4c82959dca73add69adfee9ebbc
V0V45 <118116637+V0V45@users.noreply.github.com>
2025-01-08 17:36:41 +0300

    Initial commit

---
+++ src/components/arrow-gray-button.jsx
@@ -0,0 +1,21 @@
+import arrowUpRightGray from "../images/arrow-up-right-grey.svg";
+
+export default function ArrowGrayButton({onClick, className}) {
+
+    const buttonStyle = {
+        border: "none",
+        outline: "none",
+        backgroundColor: "transparent",
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "nowrap",
+        justifyContent: "center",
+        alignItems: "center",
+}
+
+    return (
+        <button onClick={onClick} style={buttonStyle} className={className}>
+            <img src={arrowUpRightGray} alt="arrow" style={{pointerEvents: "none"}} />
+        </button>
+    );
+}
---
+++ src/components/arrow-orange-button.jsx
@@ -0,0 +1,21 @@
+import arrowDownRightOrange from "../images/arrow-down-right-orange.svg";
+
+export default function ArrowOrangeButton({onClick, className}) {
+
+    const buttonStyle = {
+        border: "none",
+        outline: "none",
+        backgroundColor: "transparent",
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "nowrap",
+        justifyContent: "center",
+        alignItems: "center",
+}
+
+    return (
+        <button onClick={onClick} style={buttonStyle} className={className}>
+            <img src={arrowDownRightOrange} alt="arrow" style={{pointerEvents: "none"}} />
+        </button>
+    );
+}
