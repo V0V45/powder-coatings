5873138fe004323970e435780b184122c6e2e17c
V0V45 <118116637+V0V45@users.noreply.github.com>
2025-01-10 23:43:49 +0300

    Moved to buttons folder

--- src/components/header/header-button/header-button.jsx
+++
@@ -1,7 +0,0 @@
-import classes from "./header-button.module.css";
-
-export default function HeaderButton({onClick, children}) {
-    return (
-        <button onClick={onClick} className={classes.button}>{children}</button>
-    );
-}
