04362b78cd9cc2e3dc10fc8bcf861016817ced08
V0V45 <118116637+V0V45@users.noreply.github.com>
2025-01-10 23:45:01 +0300

    Added filledButton and styles

--- src/components/color-search-block/color-search-block.jsx
+++ src/components/color-search-block/color-search-block.jsx
@@ -1,3 +1,4 @@
+import FilledButton from "../buttons/filled-button/filled-button";
 import TextH2 from "../text/text-h2";
 import classes from "./color-search-block.module.css";
 
@@ -5,8 +6,9 @@ export default function ColorSearchBlock() {
     return (
         <section className={classes.container}>
             <TextH2 classes={classes.h2}>Choose the paint<br />for yourself</TextH2>
-            <div className={classes.search}>
-                
+            <div className={classes.searchContainer}>
+                <FilledButton color="orange">Search</FilledButton>
+                <FilledButton color="gray">Reset filters</FilledButton>
             </div>
         </section>
     );
