3df014520788d9161a5119f872bace348cb30385
V0V45 <118116637+V0V45@users.noreply.github.com>
2025-01-10 22:31:52 +0300

    Added colorSearchBlock

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/header/header';
 import Banner from './components/banner/banner';
 import ColorfulBlock from './components/colorful-block/colorful-block';
 import WhoWeAre from './components/who-we-are/who-we-are';
+import ColorSearchBlock from './components/color-search-block/color-search-block';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Banner />
       <ColorfulBlock />
       <WhoWeAre />
+      <ColorSearchBlock />
     </>
   );
 }
